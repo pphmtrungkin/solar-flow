dbabc0488c5ca1ccd5882d7161971d62b6502ecf
pphmtrungkin <phmtrungkinn@gmail.com>
2026-03-14 08:25:40 +1100

    Convert to use Valibot instead of Zod

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
 import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod";
+import { pipe, string, url, picklist, minLength } from "valibot";
 
+const nodeEnv = ["development", "production", "test"] as const;
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().min(1),
-    BETTER_AUTH_SECRET: z.string().min(32),
-    BETTER_AUTH_URL: z.url(),
-    CORS_ORIGIN: z.url(),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    DATABASE_URL: pipe(string(), url(), minLength(3)),
+    BETTER_AUTH_SECRET: pipe(string(), minLength(32)),
+    BETTER_AUTH_URL: pipe(string(), url()),
+    CORS_ORIGIN: pipe(string(), url()),
+    NODE_ENV: picklist(nodeEnv),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -1,9 +1,8 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
-
+import { pipe, string, url } from "valibot";
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_SERVER_URL: z.url(),
+    NEXT_PUBLIC_SERVER_URL: pipe(string(), url()),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
